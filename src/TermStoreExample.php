1c145dbf185fe82b8dc409c4300156b75b189f6b
Roberto Peruzzo <roberto.peruzzo@studioaqua.it>
2019-10-16 16:05:43 +0200

    Testing TermSession class.

--- src/TermStoreExample.php
+++ src/TermStoreExample.php
@@ -9,6 +9,12 @@ use Office365\PHP\Client\SharePoint\ClientContext;
 use Office365\PHP\Client\Runtime\ClientRuntimeContext;
 use Office365\PHP\Client\SharePoint\File;
 use Office365\PHP\Client\SharePoint\SPList;
+use Office365\PHP\Client\Runtime\ClientObject;
+use Office365\PHP\Client\Runtime\CSOM\ICSOMCallable;
+use Office365\PHP\Client\Runtime\CSOM\ObjectIdentityQuery;
+use Office365\PHP\Client\Runtime\ResourcePathServiceOperation;
+use Office365\PHP\Client\SharePoint\Taxonomy\TaxonomySession;
+// use SimpleXMLElement;
 // require_once '../bootstrap.php';
 
 $settings = [
@@ -33,14 +39,16 @@ try {
     $ctx->executeQuery();
     $webUrl = $site->getProperty("Url");
 
-    $folderUrl = '/app/Costan/COSTAN_PRODUCT_RESOURCE/Gallery';
+    $path = new ResourcePathServiceOperation($ctx, null, "GetTaxonomySession", null);
+    // $path->TypeId = $this->getServerTypeId();
+    // $path->IsStatic = true;
+    $taxonomySession = new TaxonomySession($ctx, $path);
+    $objectIdentityQuery = new ObjectIdentityQuery($taxonomySession->getResourcePath());
+    $ctx->addQuery($objectIdentityQuery, $taxonomySession);
+    $ctx->addQuery($objectIdentityQuery);
+    var_dump($taxonomySession);
 
-    $url = $webUrl . "/_api/web/getFolderByServerRelativeUrl('{$folderUrl}')/Files";
-    $request = new RequestOptions($url);
-    $resp = $ctx->executeQueryDirect($request);
-    $data = json_decode($resp);
 
-    print_r($data);
 }
 catch (Exception $e) {
 	echo 'Authentication failed: ',  $e->getMessage(), "\n";
